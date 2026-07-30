6dea7f330c96a5ad60843b2641514621d21e9a00
taynaindia <taynab542@gmail.com>
2026-07-30 17:34:02 -0400

    alterações

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || '/api',
+  baseURL: import.meta.env.VITE_API_URL || 'https://onrender.com',
 });
 
 // Interceptor para adicionar token automaticamente
